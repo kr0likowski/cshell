1b8e639975900d5f424740b81b8983b9646f9660
Jedrzej Krolikowski <jedrzejkrolikowski@MacBook-Air-Jedrzej.local>
2019-01-14 16:42:48 +0100

    cd implementation

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <zconf.h>
+#include <limits.h>
 
 #define true 1
 char args[100];
@@ -10,10 +11,19 @@ char* pathVar;
 char* username;
 char* hostname;
 size_t len = 64;
+char cwd[PATH_MAX];
+char getDirectory(){
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+
+    }else{
+        perror("getcwd() error");
+    }
+}
 void execute(){
+    int status;
     pid_t pid = fork();
     if(pid==0){
-        if(execvp(NULL,NULL)<0)
+        if(execvp(*argv,argv)<0)
         {
             //wyjebalo sie;
             //error
@@ -25,14 +35,11 @@ void execute(){
         //fork sie wyjebal
         //exit 1
     }
-    /**
-     * wait pid
-     * status
-     * jak sie wyjebalo to sie wyjebalo
-     * **/
+    int w8 = waitpid(pid,&status,0);
 }
 void prompt(){
-    pathVar=getenv("PATH");
+    getDirectory();
+    pathVar=cwd;
     printf("[{%s}@{%s}{%s}]:? ",username,hostname,pathVar);
 }
 void helper(){
@@ -62,6 +69,9 @@ void getInput(char** argv, char* args)
     }
     argv[j]=NULL;
 }
+void clear(){
+
+}
 
 int main() {
     username=getenv("USER");
@@ -78,7 +88,12 @@ int main() {
             }else{
                 exit(0);
             }
+        } else if(strcmp(argv[0],"cd")==0){
+            chdir(argv[1]);
+        } else {
+            execute();
         }
+
     }
     return 0;
 }
